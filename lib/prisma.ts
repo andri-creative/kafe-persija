822eaea480b87b04d4dec112843698b8d0e8c135
Mason Rumengan\Mac <masonrumengan@yahoo.com>
2026-02-12 17:12:45 +0700

    fix: only use mysql connection string

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,30 +1,19 @@
-// import "dotenv/config";
-// import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-// import { PrismaClient } from '../generated/prisma/client';
-
-// const adapter = new PrismaMariaDb({
-//   host: process.env.DATABASE_HOST,
-//   user: process.env.DATABASE_USER,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-// });
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient({ adapter });
-// };
-
-// declare global {
-//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-// }
-
-// const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-// export default prisma;
-
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
-
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../generated/prisma/client';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
